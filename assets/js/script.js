c187c89ccb1daffc1abcc059a1aee620f9e50542
yannickferenczi <>
2023-05-04 12:28:15 +0000

    Create a riddle object in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,124 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 });
+
+/**
+ * The object Riddle picks a random string from different lists depending 
+ * on the level selected, creates a riddle out of it, calculates the amount 
+ * of points that can be won
+ */
+class Riddle {
+    constructor(level) {
+        this.level = level;
+        this.stuffToGuess = this.getStuffToGuess();
+    }
+
+    /**
+     * This method is called once when an instance is created
+     * and defines randomly what to guess
+     * @returns the word or expression to guess as a string
+     */
+    getStuffToGuess() {
+        // Fetch the listOfWords from the level pick by the user
+        // let listOfWords = fetch(`data-${this.level}.json`);
+
+        let listOfWords = [
+            "Aboil",
+            "North",
+            "Novel",
+            "Nurse",
+            "Nudge",
+            "Nerve",
+            "Nifty",
+            "Notch",
+            "Newts",
+            "Nooks",
+            "Nasal",
+            "Quirk",
+            "Quoin",
+            "Quoit",
+            "Quote",
+            "Quash",
+            "Quest",
+            "Quern",
+            "Covers",
+            "Create",
+            "Credit",
+            "Crisis",
+            "Custom",
+            "Damage",
+            "Danger",
+            "Mature",
+            "Medium",
+            "Member",
+            "Memory",
+            "Mental",
+            "Merely",
+            "Merger",
+            "Method",
+            "Middle",
+            "Miller",
+            "Mining",
+            "Report",
+            "Rescue",
+            "Resort",
+            "Result",
+            "Retail",
+            "Retain",
+            "Worker",
+            "Wright",
+            "Writer",
+            "Yachts",
+            "Yacker",
+            "Yammer",
+            "Yanked",
+            "Yapper",
+            "Yclept",
+            "Yearly",
+            "Yeasts",
+            "Yecchs",
+            "Yelled",
+            "Yodled",
+            "Yogurt",
+            "Yonder",
+            "Yttria",
+            "Yuppie"
+        ];
+
+        // Create a random Int between 0 and the length of listOfWords
+        let randomNumber = Math.floor(Math.random() * (listOfWords.length - 1));
+
+        return listOfWords[randomNumber];
+    }
+
+    /**
+     * Replace every letter of stuffToGuess and leave other characters
+     * (such as space, comma, etc) visible
+     */
+    transformStuffToGuessIntoRiddle() {
+        this.riddle = "";
+        for (let character of this.stuffToGuess) {
+            if ("abcdefghijklmnopqrstuvwxyz".includes(character.toLowerCase())) {
+                this.riddle += "_";
+            } else {
+                this.riddle += character;
+            }
+        }
+        return this.riddle;
+    }
+
+    /**
+     * Calculates the amount of points the riddle can bring
+     * if it is solved
+     * @returns a number
+     */
+    calculateRiddlePoints() {
+        this.points = 0;
+        for (let character of this.riddle) {
+            if (character === "_") {
+                this.points += 1;
+            }
+        }
+        return this.points;
+    }
+}
